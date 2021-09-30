182526ae25c54711b878d91efce1d2235a06e437
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-09-30 03:48:01 +0300

    Compress tailwind css

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
+        tailwindcss,
     ]);
